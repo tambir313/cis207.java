62b6ab3b2a7ebaf44bd340083a7c688dfca4a272
tambir313 <tambirpsn@gmail.com>
2025-01-30 19:14:12 -0500

    Add files via upload

---
+++ TableAndChairs.java
@@ -0,0 +1,22 @@
+public class TableAndChairs {
+
+    public static void main(String[] args) {
+
+        System.out.println("X                       X");
+
+        System.out.println("X                       X");
+
+        System.out.println("X       XXXXXXX         X");
+
+        System.out.println("X       X     X         X");
+
+        System.out.println("XXXXX   X     X   XXXXXXX");
+
+        System.out.println("X   X   X     X   X     X");
+
+        System.out.println("X   X   X     X   X     X");
+
+    }
+
+}
+
