8effde8a785d419f32d5d64fb27ef2d5fe53c5ba
tambir313 <tambirpsn@gmail.com>
2025-02-15 12:46:42 -0500

    Add files via upload

---
+++ SunshineMotto.java
@@ -0,0 +1,6 @@
+public class SunshineMotto {
+    public static void main(String[] args) {
+        // Display the motto
+        System.out.println("Sunshine Seashore makes it fun in the sun.");
+    }
+}
