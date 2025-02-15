96c147c3d428230a3b4ae21d27de259d6e7427b7
tambir313 <tambirpsn@gmail.com>
2025-02-15 12:38:47 -0500

    Add files via upload

---
+++ YummyMotto.java
@@ -0,0 +1,6 @@
+public class YummyMotto {
+    public static void main(String[] args) {
+        // Display the Yummy Catering motto
+        System.out.println("Yummy makes the food that makes it a party.");
+    }
+}
